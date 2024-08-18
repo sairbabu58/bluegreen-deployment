52bf16273e34775735631f40c22f0a7a46b0fbb1
BABUNI <46104458+sairbabu58@users.noreply.github.com>
2024-08-18 13:24:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 
 <title> Blue-Green Deployment </title>
 
-<!---
+
 <body bgcolor=blue text=red>
 <h1> 
 <marquee direction="right" behavior="alternate" scrollamount="20">Blue Deployment </marquee>
 </h1>
 </body>
---->
 
+
+  <!---
 <body bgcolor=green text=red>
 <h1> 
 <marquee direction="right" behavior="alternate" scrollamount="20">Green Deployment </marquee>
 </h1>
 </body>
-
+--->
 
 </html>
 
